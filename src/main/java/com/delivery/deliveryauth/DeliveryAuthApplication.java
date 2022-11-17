54e94fff6a4786ac25433857aa524e9e54bd2c72
Kostia Borozdyh <kostiaborozdyh@gmail.com>
2022-11-17 17:57:40 +0200

    initial commit

---
+++ src/main/java/com/delivery/deliveryauth/DeliveryAuthApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.deliveryauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/delivery/deliveryauth/DeliveryAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.deliveryauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
